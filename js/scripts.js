a13236ac2dd7a0beb9b04e86137bb5bc695028dc
Guy283 <111588658+Guy283@users.noreply.github.com>
2022-09-05 18:32:02 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function copyIherb() {
+    navigator.clipboard.writeText("BGG7944")
+}
+
+function copyYesStyle() {
+    navigator.clipboard.writeText("YESMAYA34")
+}
+
+function copyFeelUnique() {
+    navigator.clipboard.writeText("HANNA MAYA")
+}
+
+function copyLookFantastic() {
+    navigator.clipboard.writeText("MAYA-R1WR")
+}
