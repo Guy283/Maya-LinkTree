24cd9cf76156aa16a9d493d956732beb731b90b0
Guy283 <111588658+Guy283@users.noreply.github.com>
2022-09-07 05:04:25 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
+var copied = "הועתק ללוח";
 function copyIherb() {
     navigator.clipboard.writeText("BGG7944")
+    var copiedIherb = document.querySelector(".clickIherb")
+    copiedIherb.innerHTML = copied;
+    copiedIherb.classList.add("copied");
+
+
 }
 
 function copyYesStyle() {
     navigator.clipboard.writeText("YESMAYA34")
+    var copiedYesStyle = document.querySelector(".clickYesStyle")
+    copiedYesStyle.innerHTML = copied;
+    copiedYesStyle.classList.add("copied");
 }
 
 function copyFeelUnique() {
     navigator.clipboard.writeText("HANNA MAYA")
+    var copiedFeelUnique = document.querySelector(".clickFeelUnique")
+    copiedFeelUnique.innerHTML = copied;
+    copiedFeelUnique.classList.add("copied");
 }
 
 function copyLookFantastic() {
     navigator.clipboard.writeText("MAYA-R1WR")
+    var copiedLookFantastic = document.querySelector(".clickLookFantastic")
+    copiedLookFantastic.innerHTML = copied;
+    copiedLookFantastic.classList.add("copied");
 }
 
